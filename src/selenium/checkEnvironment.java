a8f49eb029810dd04b64aeb95c8c509de53b4e1f
Rennie <chaupoolove@gmail.com>
2021-11-24 18:42:37 +0700

    First commit

---
+++ src/selenium/checkEnvironment.java
@@ -0,0 +1,45 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class checkEnvironment {
+
+    WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		
+		
+		//Chrome
+		//System.setProperty("webdriver.chrome.driver", ".\\Lib\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", ".\\Lib\\geckodriver.exe");
+		//driver = new ChromeDriver();
+		
+//		Firefox;
+		driver = new FirefoxDriver();
+		driver.get("https://site.qr-row.staging.goquo.io/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+//
+	@Test
+	public void f() {
+		
+		String homePageTitle = driver.getTitle();
+		AssertJUnit.assertEquals(homePageTitle, "Qatar Airways Holidays | Flight + Hotel + Tour + Transfer");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
