bcb03890dd08da84841d3689ddb13e40ba8998de
Rennie <chaupoolove@gmail.com>
2021-12-02 19:14:50 +0700

    login site Etihad Dnata

--- src/selenium/checkEnvironment.java
+++ src/selenium/checkEnvironment.java
@@ -2,6 +2,7 @@ package selenium;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -25,18 +26,37 @@ public class checkEnvironment {
 		
 //		Firefox141;
 		driver = new FirefoxDriver();
-		driver.get("https://site.qr-row.staging.goquo.io/");
+		driver.get("https://test-bookuk.etihadholidays.com/agent-portal/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
  
-	@Test
-	public void f() {
+	//@Test
+	public void f() throws InterruptedException {
 		
-		String homePageTitle = driver.getTitle();
-		AssertJUnit.assertEquals(homePageTitle, "Qatar Airways Holidays | Flight + Hotel + Tour + Transfer");
+		String LoginPageTitle = driver.getTitle();
+		AssertJUnit.assertEquals(LoginPageTitle, "Login - Agent Portal");
+		driver.findElement(By.className("form-control")).sendKeys("hangtest1");
+		driver.findElement(By.id("Password")).sendKeys("Admin@123");
+		driver.findElement(By.name("Save")).click();
+		Thread.sleep(3000);
+		String CCHomepageTitle = driver.getTitle();
+		AssertJUnit.assertEquals(CCHomepageTitle, "CC Etihad Holidays");
+		
 	}
-
+	@Test
+	public void login() throws InterruptedException {
+		
+		String LoginPageTitle = driver.getTitle();
+		AssertJUnit.assertEquals(LoginPageTitle, "Login - Agent Portal");
+		driver.findElement(By.xpath("//input[@class='form-control']")).sendKeys("hangtest1");
+		driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("Admin@1234");
+		driver.findElement(By.xpath("//button[@name='Save']")).click();
+		Thread.sleep(3500);
+		String CCHomepageTitle = driver.getTitle();
+		AssertJUnit.assertEquals(CCHomepageTitle, "CC Etihad Holidays");
+		
+	}
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
